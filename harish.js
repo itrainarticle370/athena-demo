dab67057e6cd7b3f69947462a682d78620238999
manee2k6 <manee2k6@gmail.com>
2019-08-20 20:47:32 +0530

    fixed bug on harish file to deploy

--- harish.js
+++ harish.js
@@ -1,5 +1,2 @@
-sljdlfds
-flsdjfldskjfds
-fldsjfldsjfds
-fdlsjfdslfjdslf
-sdfldsjflsjdfslj
+This file is created to demo stash feature by hiding the work of mani.js
+so updating this file for demo purpose.
